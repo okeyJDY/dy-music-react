67ce289791cedb1aac4f4608bf6e5efdbe3b95ec
okeyJDY <okeyjdy@163.com>
2020-08-09 20:56:09 +0800

    使用immutable获取state中的状态数据

--- src/pages/discover/index.jsx
+++ src/pages/discover/index.jsx
@@ -1,9 +1,9 @@
 import React, { memo } from 'react';
 import { renderRoutes } from "react-router-config";
 
-import { DiscoverWrapper } from "./style";
-
 import DiscoverTopMenu from "@/components/discover-top-menu";
+
+import { DiscoverWrapper } from "./style";
 
 export default memo(function DYDiscover(props) {
   return (
