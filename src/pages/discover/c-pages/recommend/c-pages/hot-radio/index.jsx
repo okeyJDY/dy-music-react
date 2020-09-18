b2356da98f479d01dba77e28a9a8d3317b08c2c9
okeyJDY <okeyjdy@163.com>
2020-09-19 00:50:49 +0800

    完成热门主播功能, 发现音乐-推荐(discover/recommend)全部完成

--- src/pages/discover/c-pages/recommend/c-pages/hot-radio/index.jsx
+++ src/pages/discover/c-pages/recommend/c-pages/hot-radio/index.jsx
@@ -1,13 +1,34 @@
 import React, { memo } from 'react';
 
-import DYThemeHeaderSmall from '@/components/theme-header-small';
+import { hotRadios } from '@/public/local-data';
 
+import DYThemeHeaderSmall from '@/components/theme-header-small';
 import { HotRadioWrapper } from './style';
 
 export default memo(function DYHotRadio() {
   return (
     <HotRadioWrapper>
       <DYThemeHeaderSmall title="热门主播" />
+      <div className="radio-list">
+        {
+          hotRadios.map((item, index) => {
+            return (
+              <div className="item" key={item.picUrl}>
+                <a className="image" href="/todo">
+                  <img src={item.picUrl} alt=""/>
+                </a>
+                <div className="info">
+                  <div className="title">
+                    <span className="name">{item.name}</span>
+                    <span className="vip sprite_icon2"></span>
+                  </div>
+                  <div className="position text-nowrap">{item.position}</div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     </HotRadioWrapper>
   )
 })
--- src/pages/discover/c-pages/recommend/c-pages/hot-radio/style.js
+++ src/pages/discover/c-pages/recommend/c-pages/hot-radio/style.js
@@ -1,5 +1,53 @@
 import styled from 'styled-components';
 
 export const HotRadioWrapper = styled.div`
+  padding: 20px;
 
+  .radio-list {
+    margin-top: 20px;
+
+    .item {
+      display: flex;
+      width: 210px;
+      margin-bottom: 10px;
+
+      .image img {
+        width: 40px;
+        height: 40px;
+      }
+
+      .info {
+        width: 160px;
+        line-height: 21px;
+        margin-left: 10px;
+
+        .title {
+          .name {
+            color: #000;
+            margin-top: 2px;
+
+            &:hover {
+              text-decoration: underline;
+              cursor: pointer;
+            }
+          }
+
+          .vip {
+            display: inline-block;
+            position: relative;
+            top: 1px;
+            left: 3px;
+
+            width: 11px;
+            height: 13px;
+            background-position: 0 1px;
+          }
+        }
+
+        .position {
+          color: #666;
+        }
+      }
+    }
+  }
 `
